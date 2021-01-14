09aa85f8366b2ec379fcb01ef713fcc2e086ab49
Shulammite-Aso <asoamarachi@gmail.com>
2021-01-14 23:08:11 +0100

    fix installNode module

--- node.js
+++ node.js
@@ -1,4 +1,4 @@
-const { spawnSync} = require('child_process');
+const { execSync} = require('child_process');
 
 module.exports.installNode = (operatingSystem) => {
     let child;
@@ -9,72 +9,27 @@ module.exports.installNode = (operatingSystem) => {
         [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
         [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"`;
 
-        // First install node version manager (NVM)
+        // First install and load node version manager (NVM)
 
-        child = spawnSync(installNVM + " && " + loadNVM);
+        child = execSync(installNVM + " && " + loadNVM, {'encoding': 'UTF-8', 'stdio': 'inherit'});
         
-          
-         child.on('error', (error) => {
-            console("There was an error:", error);
-            process.exit();
-          });
-
           // Install node using NVM
 
-          child = spawnSync("nvm install node");
+          child = execSync("nvm install node", {'encoding': 'UTF-8', 'stdio': 'inherit'});
 
-          child.stdout.on('data', (data) => {
-            console.log(data.toString());
-          });
-          
-          child.stderr.on('data', (data) => {
-            console.error(data.toString());
-          });
-          
-          child.on('exit', (code) => {
-            console.log(`Child exited with code ${code}`);
-          });
-        
     } else if (operatingSystem === "Darwin") {
-        child = spawnSync("brew install node");
-        
-        child.stdout.on('data', (data) => {
-            console.log(`stdout: ${data}`);
-          });
-          
-         child.stderr.on('data', (data) => {
-            console.error(`stderr: ${data}`);
-          });
-          
-         child.on('exit', (code) => {
-            console.log(`child process exited with code ${code}`);
-          });
+
+        child = execSync("brew install node", {'encoding': 'UTF-8', 'stdio': 'inherit'});
         
     } else if (operatingSystem === "Windows_NT") {
 
       // First install chocolatey
-        child = spawnSync("sudo Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))");
+        child =execSync("sudo Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))", {'encoding': 'UTF-8', 'stdio': 'inherit'});
           
-         child.on('error', (error) => {
-          console("There was an error:", error);
-          process.exit();
-          });
-
           // Install node with chocolatey
 
-          child = spawnSync("cinst nodejs.install");
+          child = execSync("cinst nodejs.install", {'encoding': 'UTF-8', 'stdio': 'inherit'});
 
-          child.stdout.on('data', (data) => {
-            console.log(`stdout: ${data}`);
-          });
-          
-         child.stderr.on('data', (data) => {
-            console.error(`stderr: ${data}`);
-          });
-          
-         child.on('exit', (code) => {
-            console.log(`child process exited with code ${code}`);
-          });
     }
 
     };
